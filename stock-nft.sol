e2030d7276ed44bc7921baa5eefa35fe2cc0fe8e
nc-collin <collinsuwono@gmail.com>
2022-01-24 07:31:01 +0800

    Update stock-nft.sol

--- stock-nft.sol
+++ stock-nft.sol
@@ -12,8 +12,6 @@ contract stock_nft is ERC1155 {
     uint board_member_count;
     string public url;
     uint public origin_token_count;
-    uint public min_board_members;
-    string public founding_date;
 
     uint private token_id = 0;
     uint public proposal_count = 1;
@@ -24,6 +22,7 @@ contract stock_nft is ERC1155 {
     struct Founder {
         address founder_address;
         uint founder_token_id;
+        string founding_date;
     }
 
     struct Proposal {
@@ -39,7 +38,7 @@ contract stock_nft is ERC1155 {
     mapping(address => bool) public signed;
     mapping(uint => mapping(address => bool)) public isConfirmed;
 
-    constructor(string memory _company_name, address[] memory _board_members,
+    constructor(string memory _company_name, address[] memory _board_members, 
         uint _token_count, string memory _founding_date, address _system_address) public  ERC1155("https://raw.githubusercontent.com/nc-collin/seed/master/{i}.json?token=GHSAT0AAAAAABQE7EOBLELIQHXMHGBWAXVYYPN263A"){
 
         company_name = _company_name;
@@ -47,15 +46,14 @@ contract stock_nft is ERC1155 {
         board_member_count = board_members.length;
         origin_token_count = _token_count;
         url = "https://raw.githubusercontent.com/nc-collin/seed/master/{i}.json?token=GHSAT0AAAAAABQE7EOBLELIQHXMHGBWAXVYYPN263A";
-        founding_date = _founding_date;
 
         for (uint i=0; i < board_member_count; ++i) {
             is_board_member[board_members[i]] = true;
         }
 
-        founder = Founder(msg.sender, token_id);
+        founder = Founder(msg.sender, token_id, _founding_date);
 
-        system_address = _system_address;
+        system_address = _system_address;        
     }
 
     modifier isBoardMember() {
@@ -73,20 +71,20 @@ contract stock_nft is ERC1155 {
         return proposal_count++;
     }
 
-    function agreeIssue(uint proposalID) external isBoardMember {
-        require(!isConfirmed[proposalID][msg.sender], "You have agreed!");
-        isConfirmed[proposalID][msg.sender] = true;
-        Proposals[proposalID].agreed_count += 1;
+    function confirmProposal(uint proposal_id) external isBoardMember {
+        require(!isConfirmed[proposal_id][msg.sender], "You already confirmed this proposal");
+        isConfirmed[proposal_id][msg.sender] = true;
+        Proposals[proposal_id].agreed_count += 1;
     }
 
-    function stockIssue(uint proposalID, uint _token_id) public founderPrivilege isBoardMember {
-        require(Proposals[proposalID].agreed_count == board_member_count);
+    function stockIssue(uint proposal_id, uint _token_id) public founderPrivilege isBoardMember {
+        require(Proposals[proposal_id].agreed_count == board_member_count);
         _mint(system_address, _token_id, origin_token_count, "");
         token_id++;
     }
 
-    function stockReissue(uint proposalID, uint _token_id, uint amount) public founderPrivilege isBoardMember {
-        require(Proposals[proposalID].agreed_count == board_member_count);
+    function stockReissue(uint proposal_id, uint _token_id, uint amount) public founderPrivilege isBoardMember {
+        require(Proposals[proposal_id].agreed_count == board_member_count);
         _mint(system_address, _token_id, amount, "");
         token_id++;
     }
